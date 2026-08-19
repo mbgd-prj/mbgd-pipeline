d7d5057625d02eda4f7317da94e343f4204bcf52
exo <exo@ccfep3.center.ims.ac.jp>
2026-08-19 12:09:44 +0900

    Add PATH setting to etc/bashrc in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # install domclust
 MPL_TOPDIR=`pwd`
-echo "export \MPL_TOPDIR=$MPL_TOPDIR" > etc/bashrc
+echo "export MPL_TOPDIR=$MPL_TOPDIR" > etc/bashrc
+echo "PATH=\$MPL_TOPDIR/bin:\$PATH" >> etc/bashrc
 cd package/
-git clone https://github.com/mbgd-prj/domclust.git
-cd domclust
-./configure
-make
+if [ ! -f domclust/bin/domclust ]; then
+	git clone https://github.com/mbgd-prj/domclust.git
+	cd domclust
+	./configure --prefix=$MPL_TOPDIR
+	make  install
+fi
